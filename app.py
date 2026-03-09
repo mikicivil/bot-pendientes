cbfc4dcee31e9b54f4703fa0d9b91d3c1afefc18
mikicivil <98473261+mikicivil@users.noreply.github.com>
2026-03-09 17:47:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -350,4 +350,4 @@ def health():
 if __name__ == "__main__":
     inicializar_excel()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True) 
